a29b0d569690e2fa1af5ec2e68e003d5862e8282
Catherin1 <2021302131019@whu.edu.cn>
2023-05-05 10:46:50 +0800

    xiaochengxu

--- test/geo_operation.py
+++ test/geo_operation.py
@@ -1,22 +1,19 @@
 from pymongo import MongoClient
 
+# 访问lulinlin数据库
 client = MongoClient("mongodb://localhost:27017/")
 db = client["lulinlin"]
 coll = db["lulinlin_col"]
- 
+
 # x = coll.find_one()
 # print(x)
 
+# 创建索引
 coll.create_index([("geometry", "2dsphere")])
-
-# coll.create_index([("geometry", "2dsphere")])
-
-
-filter = {"geometry": {"$geoIntersects": {"$geometry": {"type": "LineString","coordinates":
-     [[84.06,26.18], [109.56,46.02]]}} } }
-projection = {"properties":1}
-
-# for doc in coll.find(filter, projection):
-#     print(doc)
-
+# 用直线（84.06，26.18）--（109.56，46.02）进行intersect查询
+doc = coll.find({
+    "geometry": {"$geoIntersects": {"$geometry": {"type": "LineString", "coordinates":
+        [[84.06, 26.18], [109.56, 46.02]]}}}}, {"properties": 1})
+print("查询结果属性信息为：{}".format(doc))
+# 关闭数据库连接
 client.close()
